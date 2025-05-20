cb2db457e056b2e040606fe8b2e2d54c9621a135
Sky <126008711+sumitmishra01@users.noreply.github.com>
2025-05-20 12:26:24 +0530

    Add implementation for Two Sum problem with example usage

---
+++ Collections/problemset/_03_TwoSum.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {-1, -1};
+    }
+}
+
+public class _03_TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);  // Should print: 0, 1
+    }
+}
